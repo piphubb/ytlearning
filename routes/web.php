f352ab5de016d81e5427c9c1c31e626a2ea8e4b7
piphub <piphub@piphub22s-MacBook-Pro.local>
2023-08-16 14:15:32 +0700

    laravel learning

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome',[
+        'name' => 'Piphub'
+    ]);
+});
+//Route::get('/home', function (){
+//   return view('pages.home');
+//});
+Route::get('/home',[HomeController::class,'index']);
+
+//invokable
+Route::get('/post', PostController::class);
