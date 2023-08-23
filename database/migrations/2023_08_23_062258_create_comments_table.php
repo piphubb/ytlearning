8f567c33491b2ecfcd3b76db5f52f584e19ec297
piphubb <piphub.p16@gmail.com>
2023-08-23 15:21:54 +0700

    fix foriegn key

--- database/migrations/2023_08_23_062258_create_comments_table.php
+++ database/migrations/2023_08_23_062258_create_comments_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->integer('post_id')->unsigned();
+            $table->unsignedBigInteger("post_id");
             $table->string('comment');
-            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('post_id')->references('id')->on('posts');
             $table->timestamps();
         });
     }
--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -17,11 +17,11 @@ class PostSeeder extends Seeder
     {
         DB::table('posts')->insert([
             'title' => 'Create crud application using Laravel 9',
-            'content' => " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+            'content' => "when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
         ]);
         DB::table('posts')->insert([
             'title' => 'Create crud application using Laravel 9',
-            'content' => " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+            'content' => "when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
         ]);
     }
 }
